3723d655270aba57ba7c73f6259559d1005579fb
test <test@test.test>
2023-09-06 09:59:19 +0000

    set version to 2.9.0

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 provider "kubernetes" {
-  config_path    = "~/.kube/config" # chemin vers le fichier de configuration de Kubernetes
-  config_context = "microservices" # le contexte utilisé pour les charges de travail à déployer
-}
+  config_path    = "~/.kube/config" # chemin vers le fichier de configuration de kubernetes
+    config_context = "microservices" # le context utilisé pour les charges de travails à déployer
+      }
+
+  terraform {
+   required_providers {
+           kubernetes = {
+                 source  = "hashicorp/kubernetes"
+                       version = "~> 2.9.0"
+                        }
+                      }
+            }
+
